d78072a3524e4a778cc332f8b4d62f64f8563d0c
Joseph Hooker <jcolehooker@gmail.com>
2021-11-06 16:36:59 -0500

    Finished template, routing - basic assignment complete

--- users_templates_app/views.py
+++ users_templates_app/views.py
@@ -1,11 +1,15 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+# from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    context = {
-
+    context={
+        'users':User.objects.all()
     }
-
     return render(request, "index.html", context)
-    # return HttpResponse("this")
+
+def createUser(request):
+    User.objects.create(first_name=request.POST['first_name'],
+    last_name=request.POST['last_name'], email=request.POST['email'], age=request.POST['age'])
+    return redirect('/')
